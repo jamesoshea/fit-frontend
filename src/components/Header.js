30cdee887f9805d5412910f0cd8b9199bd3344b2
James O'Shea <jamesoshea89@hotmail.com>
2018-06-17 20:24:25 +0200

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <FitHeader>Hello</FitHeader>
+        <FitSubheader>track your habits</FitSubheader>
+        <hr />
+      </div>
+    );
+  }
+}
+
+const FitHeader = styled.h1`
+  text-align: center;
+`;
+
+const FitSubheader = styled.h3`
+  text-align: center;
+`;
+
+export default Header;
